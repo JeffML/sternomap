180aebc0607bcfc960a1c1839f5480e22c5229f2
Jeff Lowery <jeff.l@mavericklabel.com>
2019-04-02 16:37:29 -0700

    tabular, dude

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Canvas from './Canvas';
+import SternoMap from './SternoMap';
 
 class App extends Component {
   render() {
@@ -12,7 +12,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <Canvas/>
+          <SternoMap/>
           <a
             className="App-link"
             href="https://reactjs.org"
---
+++ src/SternoMap.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import Sterno from "./Sterno";
+import casual from "casual-browserify";
+
+const getNames = (count) => {
+  const set = new Set()
+  for (let i = 0; i < count; i++) {
+    set.add(casual.name)
+  }
+  return Array.from(set);
+}
+
+const getValues = (count) => {
+  const arr = [];
+  for (let i = 0; i < count; i++) {
+    arr.push(Math.random())
+  }
+  return arr;
+}
+
+const values = {
+  y: getNames(30),
+  x: getValues(300)
+}
+
+const cols = () => values.x.map(x => <td>1</td> );
+
+const rows = () => 
+  values.y.map( y => <tr>
+    {cols()}
+    </tr>
+  );
+
+const SternoMap = () => {
+  return (
+    <div>
+      <table>
+        <tbody>
+          {rows()}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default SternoMap;
