6234cfc36cba132c7628f3f7405302e2b033e003
Jeff Lowery <jeff.l@mavericklabel.com>
2019-04-03 17:34:39 -0700

    notes

--- src/App.js
+++ src/App.js
@@ -8,19 +8,21 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
-          </p>
+          </p> */}
+          <p>
           <SternoMap/>
-          <a
+          </p>
+          {/* <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
+          </a> */}
         </header>
       </div>
     );
